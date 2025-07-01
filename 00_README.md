9f4e56bb5558f5d6c7447b4ca21452f516cb37e9
Abhishek Nema <nemaabhishek3@gmail.com>
2025-07-01 20:53:09 +0530

    added DOM

---
+++ 00_README.md
@@ -0,0 +1,49 @@
+Topics covered...
+
+- Normal Function
+
+- Arrow Function
+
+- Higher Order Function, Callback function
+
+- Array Higher Order Function
+  -- forEach loop
+  -- map()
+  -- find()
+  -- filter()
+  -- slice()
+  -- splice()
+
+- Intro to DOM
+  -- alert()
+  -- prompt()
+  -- window object i.e. window.console.log
+  -- window.document, window.document.title, window.document.write
+
+- QuerySelector - DOM
+  -- document.querySelector("tagName")
+  -- innerHTML
+  -- innerText
+  -- children, parentElement
+  -- document.querySelectorAll("tagName")
+
+- document.getElementById()
+  -- classList.add
+  -- classList.remove
+  -- also adding inline styles using js
+  -- variableName.setAttribute()
+  -- variableName.remove()
+
+- document.getElementsByClassName()
+
+- Object Destructuring
+  -- Object Destructuring
+  -- multi-level de-structuring
+
+- Array destructuring
+  -- Array Destructuring
+  -- De-structuring array as an object
+
+- Destructuring in function parameter
+
+- Optional Chaining i.e. ?.
---
+++ 04_Arrays_HOF.js
@@ -0,0 +1,37 @@
+const numArray = [1, 2, 3, 4, 5, 6];
+
+const doubleNum = function (num) {
+  return num * 2;
+};
+
+// forEach loop - iterates over each element but kuch return nhi karta
+let res1 = numArray.forEach((n) => {
+  console.log(n * 2);
+});
+console.log(res1);
+
+// map() - iterates over each element and returns a new array
+let res2 = numArray.map(doubleNum);
+console.log(res2);
+
+// find() - it searches for the first element based on satisfied condition and returns that element
+let numbersArray = [1, 3, 2, 4, 5, 4];
+let answer = numbersArray.find((n) => n === 4);
+console.log(answer);
+
+// filter() - iterates over each array elements, and returns new array based on satisfied condition
+numbersArray = [10, 30, 20, 40, 50, 40];
+const filteredArray = numbersArray.filter((n) => n > 21);
+console.log(filteredArray);
+
+// slice() - it returns new array by copying elements from original array
+let fruitArray = ["mango", "apple", "banana", "papaya", "melon", "cherry"];
+let slicedArray = fruitArray.slice(1, 4); // means index(1) se index(4-1) tak elements
+console.log(fruitArray);
+console.log(slicedArray);
+
+// splice() - it modifies original array, by adding or removing from it
+let vegeiesArray = ["tomato", "potato", "cauliflower", "spinach", "ladyfinger"];
+let splicedArray = vegeiesArray.splice(1, 0, "😎"); // means start with index(1), delete 0 elements, add "😎"
+console.log(splicedArray);
+console.log(vegeiesArray);
